8c2810407338ef0925ee1b8d74073784b3e001d2
Henke Adolfsson <catears13@gmail.com>
2022-07-03 09:01:35 +0200

    Remove basic test program

--- Client/WebServer/BasicWebApi.Tester/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using BasicWebApi.SMCManifest;
-
-Console.WriteLine("Hello, World!");
-
-var manifest = new Manifest();
-Console.WriteLine("Exe found at: " + manifest.GetExeLocator().GetExeFileLocation());
